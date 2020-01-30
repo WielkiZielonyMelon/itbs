c105be6f06a4592a2b93f0d7211fdb2403663a75
Melon <wielkizielonymelon@protonmail.com>
2020-01-30 18:19:12 +0100

    Add FireTile to pool of tiles that can't be damaged

--- src/helpers/is_tile_damagable.py
+++ src/helpers/is_tile_damagable.py
@@ -1,8 +1,8 @@
-from src.game_objects.tiles.tile import GroundTile, WaterTile, AcidTile, LavaTile, ForestFireTile
+from src.game_objects.tiles.tile import GroundTile, WaterTile, AcidTile, LavaTile, ForestFireTile, FireTile
 
 
 def is_tile_damageable(tile):
-    if isinstance(tile, (GroundTile, WaterTile, AcidTile, LavaTile, ForestFireTile)):
+    if isinstance(tile, (GroundTile, WaterTile, AcidTile, LavaTile, ForestFireTile, FireTile)):
         return False
     else:
         return True
