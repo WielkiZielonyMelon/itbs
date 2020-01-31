b55a21533d162e6823eb80b58cde2d07cc37265a
Melon <wielkizielonymelon@protonmail.com>
2020-01-31 20:01:42 +0100

    Mechs were blocked by other mechs

--- src/helpers/get_possible_moves.py
+++ src/helpers/get_possible_moves.py
@@ -9,7 +9,10 @@ def get_possible_moves(board, obj):
 
     # Check if object is still present on board, if not, return empty array of possible moves
     pos = board.find_object_position(obj)
+    # Holder for visited moves
     possible_moves = []
+    # Holder for position that were visited (however not possible to move there)
+    visited = []
     # Queue hold a tuple, current player controlled object position
     # and how many moves are left from that position
     q = Queue()
@@ -30,8 +33,13 @@ def get_possible_moves(board, obj):
         #              West     East    South    North
         for vector in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
             new_pos = (pos_x + vector[0], pos_y + vector[1])
-            if board.in_bounds(new_pos) and not board[new_pos].has_object() and new_pos not in possible_moves:
-                possible_moves.append(new_pos)
-                q.put((new_pos, moves_left))
+            if board.in_bounds(new_pos) and new_pos not in possible_moves:
+                obj = board[new_pos].get_object()
+                if obj is None:
+                    possible_moves.append(new_pos)
+                    q.put((new_pos, moves_left))
+                elif new_pos not in visited and obj.is_player_controlled():
+                    visited.append(new_pos)
+                    q.put((new_pos, moves_left))
 
     return possible_moves
--- tests/test_get_possible_moves.py
+++ tests/test_get_possible_moves.py
@@ -4,6 +4,8 @@ import pytest
 
 from src.game_objects.board import Board
 from src.game_objects.mech import CombatMech
+from src.game_objects.mountain import Mountain
+from src.game_objects.vek import Firefly
 from src.helpers.get_possible_moves import get_possible_moves
 
 
@@ -33,7 +35,28 @@ def test_expected_moves(pos, mobility, expected_moves):
                           ((7, 7), 2, (7, 6), [(6, 7), (5, 7), (6, 6)]),
                           ((0, 7), 2, (1, 7), [(0, 6), (0, 5), (1, 6)]),
                           ((0, 7), 2, (0, 6), [(1, 7), (2, 7), (1, 6)])])
-def test_expected_moves_with_blocker(pos_start, mobility, pos_block, expected_moves):
+def test_expected_moves_with_blocker_vek(pos_start, mobility, pos_block, expected_moves):
+    board = Board()
+    mech = CombatMech()
+    mech._moves = mobility
+    vek = Firefly()
+
+    board[pos_start].set_object(mech)
+    board[pos_block].set_object(vek)
+    moves = get_possible_moves(board, mech)
+    unittest.TestCase().assertCountEqual(moves, expected_moves)
+
+
+@pytest.mark.parametrize("pos_start, mobility, pos_block, expected_moves",
+                         [((0, 0), 2, (1, 0), [(0, 1), (1, 1), (0, 2), (2, 0)]),
+                          ((0, 0), 2, (0, 1), [(1, 0), (1, 1), (2, 0), (0, 2)]),
+                          ((7, 0), 2, (6, 0), [(7, 1), (7, 2), (6, 1), (5, 0)]),
+                          ((7, 0), 2, (7, 1), [(6, 0), (5, 0), (6, 1), (7, 2)]),
+                          ((7, 7), 2, (6, 7), [(7, 6), (7, 5), (6, 6), (5, 7)]),
+                          ((7, 7), 2, (7, 6), [(6, 7), (5, 7), (6, 6), (7, 5)]),
+                          ((0, 7), 2, (1, 7), [(0, 6), (0, 5), (1, 6), (2, 7)]),
+                          ((0, 7), 2, (0, 6), [(1, 7), (2, 7), (1, 6), (0, 5)])])
+def test_expected_moves_with_blocker_mech(pos_start, mobility, pos_block, expected_moves):
     board = Board()
     mech = CombatMech()
     mech._moves = mobility
@@ -45,12 +68,15 @@ def test_expected_moves_with_blocker(pos_start, mobility, pos_block, expected_mo
     unittest.TestCase().assertCountEqual(moves, expected_moves)
 
 
-def test_2_move_1_1_blocked_fully():
+@pytest.mark.parametrize("blocker, exp_moves",
+                         [(CombatMech, [(0, 0), (2, 0), (3, 1), (0, 2), (2, 2), (1, 3)]),
+                          (Firefly, []),
+                          (Mountain, [])])
+def test_2_move_1_1_blocked(blocker, exp_moves):
     mobility = 2
     board = Board()
     mech = CombatMech()
     mech._moves = mobility
-    mech_blocker = CombatMech()
 
     pos_start = (1, 1)
     pos_block_0 = (0, 1)
@@ -58,10 +84,32 @@ def test_2_move_1_1_blocked_fully():
     pos_block_2 = (1, 0)
     pos_block_3 = (1, 2)
     board[pos_start].set_object(mech)
-    board[pos_block_0].set_object(mech_blocker)
-    board[pos_block_1].set_object(mech_blocker)
-    board[pos_block_2].set_object(mech_blocker)
-    board[pos_block_3].set_object(mech_blocker)
+    board[pos_block_0].set_object(blocker())
+    board[pos_block_1].set_object(blocker())
+    board[pos_block_2].set_object(blocker())
+    board[pos_block_3].set_object(blocker())
     moves = get_possible_moves(board, mech)
-    expected_moves = []
+#    expected_moves =
+    unittest.TestCase().assertCountEqual(moves, exp_moves)
+
+def test_moves_with_blocked_mech():
+    board = Board()
+    mech00 = CombatMech()
+    mech00_pos = (0, 0)
+    mech01 = CombatMech()
+    mech01_pos = (1, 0)
+
+    board[mech00_pos].set_object(mech00)
+    board[mech01_pos].set_object(mech01)
+    board[(0, 1)].set_object(Mountain())
+    board[(1, 1)].set_object(Mountain())
+    board[(2, 1)].set_object(Mountain())
+    board[(3, 0)].set_object(Mountain())
+    board[(3, 1)].set_object(Mountain())
+
+    expected_moves = [(2, 0)]
+    moves = get_possible_moves(board, mech00)
+    unittest.TestCase().assertCountEqual(moves, expected_moves)
+
+    moves = get_possible_moves(board, mech01)
     unittest.TestCase().assertCountEqual(moves, expected_moves)
