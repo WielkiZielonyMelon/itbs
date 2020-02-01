e0a9490aee71a4700fe0e16d97002ebaa5afce7e
Melon <wielkizielonymelon@protonmail.com>
2020-02-01 20:46:46 +0100

    Add tile copying in apply_attack_acid

--- src/apply_attack/apply_attack_acid.py
+++ src/apply_attack/apply_attack_acid.py
@@ -1,5 +1,11 @@
+import copy
+
 from src.apply_attack.apply_attack import acidify_tile
 
 
 def apply_attack_acid(board, attack):
+    pos = attack.get_attacker()
+    ret = {pos: copy.deepcopy(board[pos])}
     acidify_tile(board, attack.get_attacker())
+
+    return ret
