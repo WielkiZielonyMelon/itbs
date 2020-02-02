6d728a79da2dd22954badc857b661eb59d74cda4
Melon <wielkizielonymelon@protonmail.com>
2020-02-02 20:44:42 +0100

    apply_environment can use copy of cached objects

--- src/helpers/apply_environment.py
+++ src/helpers/apply_environment.py
@@ -1,3 +1,5 @@
+import copy
+
 from src.apply_attack.apply_attack_burn import apply_attack_burn
 from src.game_objects.attack import Attack
 from src.game_objects.weapons.burn import Burn
@@ -8,14 +10,13 @@ def apply_environment(board):
     """Looks for any fire, storm clouds and regenerating psions and applies their effects"""
     d = {}
     # First, look for fire
-    for row in range(0, board.BOARD_X_SIZE):
-        for col in range(0, board.BOARD_Y_SIZE):
-            obj = board[(row, col)].get_object()
-            if obj is None:
-                continue
-            if obj.is_on_fire():
-                attack = Attack(attacker=(row, col), weapon=Burn(), vector=None)
-                update_dict_if_key_not_present(d, apply_attack_burn(board, attack))
+    for _, (x, y) in copy.copy(board._object_position_cache).items():
+        obj = board[(x, y)].get_object()
+        if obj is None:
+            continue
+        if obj.is_on_fire():
+            attack = Attack(attacker=(x, y), weapon=Burn(), vector=None)
+            update_dict_if_key_not_present(d, apply_attack_burn(board, attack))
 
     # TODO: Add storm clouds damage
     # TODO: Add psion attack
