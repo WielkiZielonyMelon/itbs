65a805f33808d74b25619b57dc0d293527269ab2
Melon <wielkizielonymelon@protonmail.com>
2020-02-03 09:13:32 +0100

    Limit object retrieval in apply_attack_push

--- src/apply_attack/apply_attack_push.py
+++ src/apply_attack/apply_attack_push.py
@@ -8,10 +8,11 @@ from src.helpers.update_dict_if_key_not_present import update_dict_if_key_not_pr
 
 
 def apply_attack_push(board, attack):
-    push_pos = attack.get_attacker()
-    if not board.in_bounds(push_pos):
+    push_from = attack.get_attacker()
+    if not board.in_bounds(push_from):
         return {}
-    object_to_push = board[push_pos].get_object()
+    push_from_tile = board[push_from]
+    object_to_push = push_from_tile.get_object()
     # If there is no object, than nothing can be moved
     if object_to_push is None:
         return {}
@@ -22,28 +23,25 @@ def apply_attack_push(board, attack):
         return {}
 
     direction = attack.get_vector()
-    new_pos = (push_pos[0] + direction[0], push_pos[1] + direction[1])
+    push_to = (push_from[0] + direction[0], push_from[1] + direction[1])
 
     # If out of bounds, do not push object
-    if not board.in_bounds(new_pos):
+    if not board.in_bounds(push_to):
         return {}
 
     # If not obstructed, push!
-    if not board[new_pos].has_object():
-        attack = Attack(attacker=object_to_push.get_id(), weapon=Move(), vector=new_pos)
-        return apply_attack_move(board, attack, push_pos)
+    push_to_tile = board[push_to]
+    blocker = push_to_tile.get_object()
+    if blocker is None:
+        attack = Attack(attacker=object_to_push.get_id(), weapon=Move(), vector=push_to)
+        return apply_attack_move(board, attack, push_from)
 
     # Destination is obstructed
-    ret = {new_pos: copy.deepcopy(board[new_pos]), push_pos: copy.deepcopy(board[push_pos])}
-    board[new_pos].bump_damage(1)
-    board[push_pos].bump_damage(1)
+    ret = {push_to: copy.deepcopy(push_to_tile), push_from: copy.deepcopy(push_from_tile)}
+    board[push_to].bump_damage(1)
+    board[push_from].bump_damage(1)
 
-    tile = board[new_pos]
-    obj = tile.get_object()
-    update_dict_if_key_not_present(ret, kill_object_if_possible(board, new_pos, obj))
-
-    tile = board[push_pos]
-    obj = tile.get_object()
-    update_dict_if_key_not_present(ret, kill_object_if_possible(board, push_pos, obj))
+    update_dict_if_key_not_present(ret, kill_object_if_possible(board, push_to, blocker))
+    update_dict_if_key_not_present(ret, kill_object_if_possible(board, push_from, object_to_push))
 
     return ret
