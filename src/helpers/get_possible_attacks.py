4fc9b4b1041666786a73b632b189c6870d8efcc8
Melon <wielkizielonymelon@protonmail.com>
2020-02-20 19:25:43 +0100

    Reduce object and object id retrieval

--- src/helpers/get_possible_attacks.py
+++ src/helpers/get_possible_attacks.py
@@ -9,66 +9,64 @@ from src.game_objects.weapons.titan_fist import TitanFist
 
 
 def get_possible_attacks(board, attacker_pos):
-    """Return a list of possible attacks. If provided object is None, exception will be risen.
-    If object's position cannot be found, exception will be risen"""
-    # Get attackers position, that still might be removed from the board
-    obj = board[attacker_pos].get_object()
-    # If attacker is in liquid tile, there are no possible attacks
-    if board[attacker_pos].is_liquid() and not obj.is_flying():
+    """Return a list of possible attacks for object in provided position"""
+    tile = board[attacker_pos]
+    attacker = tile.get_object()
+    # Non-flying object in water can't attack
+    if tile.is_liquid() and not attacker.is_flying():
         return []
 
     # If attacker is in smoke, there is no possibility to attack
-    if board[attacker_pos].has_smoke():
+    if tile.has_smoke():
         # TODO: If Camila Vera is piloting, she can still attack
         return []
 
     # Right, now create a list of possible attacks
     possible_attacks = []
-    for weapon in obj.get_weapons():
+    for weapon in attacker.get_weapons():
         if isinstance(weapon, Repair):
-            possible_attacks.extend(get_possible_attacks_repair(board, weapon, attacker_pos))
+            possible_attacks.extend(get_possible_attacks_repair(board, weapon, attacker, attacker_pos))
         elif isinstance(weapon, TitanFist):
-            possible_attacks.extend(get_possible_attacks_titan_fist(board, weapon, attacker_pos))
+            possible_attacks.extend(get_possible_attacks_titan_fist(board, weapon, attacker, attacker_pos))
         elif isinstance(weapon, AerialBombs):
-            possible_attacks.extend(get_possible_attacks_aerial_bombs(board, weapon, attacker_pos))
+            possible_attacks.extend(get_possible_attacks_aerial_bombs(board, weapon, attacker, attacker_pos))
         elif isinstance(weapon, Artemis):
-            possible_attacks.extend(get_possible_attacks_artemis(board, weapon, attacker_pos))
+            possible_attacks.extend(get_possible_attacks_artemis(board, weapon, attacker, attacker_pos))
         elif isinstance(weapon, TaurusCannon):
-            possible_attacks.extend(get_possible_attacks_taurus_cannon(board, weapon, attacker_pos))
+            possible_attacks.extend(get_possible_attacks_taurus_cannon(board, weapon, attacker, attacker_pos))
         else:
             raise Exception("Do not know this type of weapon!")
 
     return possible_attacks
 
 
-def get_possible_attacks_repair(board, weapon, attacker_pos):
+def get_possible_attacks_repair(board, weapon, attacker, attacker_pos):
     """Returns all possible repair attacks"""
-    return [Attack(attacker=board[attacker_pos].get_object().get_id(),
-                   weapon=weapon, vector=None)]
+    return [Attack(attacker=attacker.get_id(), weapon=weapon, vector=None)]
 
 
-def get_directional_attack_vectors(board, weapon, attacker_pos):
+def get_directional_attack_vectors(board, weapon, attacker, attacker_pos):
     possible_attacks = []
+    attacker_id = attacker.get_id()
     # This is a list of relative positions that can be pushed
     for vector in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
         attacked_pos = (attacker_pos[0] + vector[0], attacker_pos[1] + vector[1])
         if not board.in_bounds(attacked_pos):
             continue
 
-        new_attack = Attack(attacker=board[attacker_pos].get_object().get_id(),
-                            weapon=weapon, vector=vector)
+        new_attack = Attack(attacker=attacker_id, weapon=weapon, vector=vector)
         possible_attacks.append(new_attack)
 
     return possible_attacks
 
 
-def get_possible_attacks_titan_fist(board, weapon, attacker_pos):
-    return get_directional_attack_vectors(board, weapon, attacker_pos)
+def get_possible_attacks_titan_fist(board, weapon, attacker, attacker_pos):
+    return get_directional_attack_vectors(board, weapon, attacker, attacker_pos)
 
 
-def get_possible_attacks_aerial_bombs(board, weapon, attacker_pos):
+def get_possible_attacks_aerial_bombs(board, weapon, attacker, attacker_pos):
     possible_attacks = []
-    attacker = board[attacker_pos].get_object()
+    attacker_id = attacker.get_id()
 
     rnge = weapon.get_full_range()
     vectors = []
@@ -83,15 +81,14 @@ def get_possible_attacks_aerial_bombs(board, weapon, attacker_pos):
         if board[landing_pos].has_object():
             continue
 
-        new_attack = Attack(attacker=attacker.get_id(), weapon=weapon, vector=vector)
+        new_attack = Attack(attacker=attacker_id, weapon=weapon, vector=vector)
         possible_attacks.append(new_attack)
 
     return possible_attacks
 
 
-def get_possible_attacks_artemis(board, weapon, attacker_pos):
+def get_possible_attacks_artemis(board, weapon, attacker, attacker_pos):
     possible_attacks = []
-    attacker = board[attacker_pos].get_object()
     attacker_pos_x = attacker_pos[0]
     attacker_pos_y = attacker_pos[1]
 
@@ -101,12 +98,13 @@ def get_possible_attacks_artemis(board, weapon, attacker_pos):
     vectors.extend([(x, attacker_pos_y) for x in itertools.chain(range(0, attacker_pos_x - 1),
                                                                  range(attacker_pos_x + 2, board.BOARD_X_SIZE))])
 
+    attacker_id = attacker.get_id()
     for vector in vectors:
-        possible_attacks.append(Attack(attacker=attacker.get_id(), weapon=weapon,
+        possible_attacks.append(Attack(attacker=attacker_id, weapon=weapon,
                                        vector=(vector[0] - attacker_pos_x, vector[1] - attacker_pos_y)))
 
     return possible_attacks
 
 
-def get_possible_attacks_taurus_cannon(board, weapon, attacker_pos):
-    return get_directional_attack_vectors(board, weapon, attacker_pos)
+def get_possible_attacks_taurus_cannon(board, weapon, attacker, attacker_pos):
+    return get_directional_attack_vectors(board, weapon, attacker, attacker_pos)
